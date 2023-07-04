eb8fa07e2d194d1602a7338673421a5860934550
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-04 08:03:52 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (num1, num2) {
+    return num1 / num2;
+}
+
+const square = (num) => {
+    return num * num;
+}
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
